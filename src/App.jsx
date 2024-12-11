84290c41e790687855769faa78c0dea93afa4c6b
mintruik <128770488+mintruik@users.noreply.github.com>
2024-12-12 01:09:07 +0530

    Fix import paths for SubjectsBrief component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ExamSchedule from './Components/ExamSchedule/ExamSchedule.jsx'
-import SubjectsBrief from './components/Bachelor of Arts (Honors Economics)/Semester - III - Eco - Honour/Subjects_Brief';
+import SubjectsBrief from './components/Bachelor of Arts (Honors Economics)/Semester - III - Eco - Honour/subjects_brief';
 import EconomicHistory from './components/Bachelor of Arts (Honors Economics)/Semester - III - Eco - Honour/Economic History of India 1857-1947';
 import { examData } from './data/examData';
 
